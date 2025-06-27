3817ee1afa3e098a3fbc3cf377c98a032bfc690b
Anirudhhan <anirudhhan.ashok@gmail.com>
2025-06-27 16:36:32 +0530

    reload replaced

--- src/components/PdfListComponent.tsx
+++ src/components/PdfListComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardAction,
@@ -8,18 +8,29 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
-import { Trash } from "lucide-react";
+import { LoaderCircle, Trash } from "lucide-react";
 import { toast } from "sonner";
 
+interface PdfListComponentProps extends PDFFile {
+  onDelete: () => void;
+}
 
-const PdfListComponent = ({ id, file_name, created_at, user_id }: PDFFile) => {
+const PdfListComponent = ({
+  id,
+  file_name,
+  created_at,
+  user_id,
+  onDelete,
+}: PdfListComponentProps) => {
   const formattedDate = new Date(created_at);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   console.log(id, formattedDate);
 
   const deletePdf = async () => {
     const confirmDelete = confirm("Are you sure you want to delete this PDF?");
     if (!confirmDelete) return;
+    setIsDeleting(true);
 
     try {
       const res = await fetch("/api/pdf/delete", {
@@ -37,11 +48,12 @@ const PdfListComponent = ({ id, file_name, created_at, user_id }: PDFFile) => {
 
       toast.success("PDF deleted successfully");
 
-      // Optional: Refresh page or call a prop to update state
-      window.location.reload();
+      onDelete();
     } catch (error) {
       console.error("Delete error:", error);
       toast.error("Something went wrong");
+    } finally {
+      setIsDeleting(false);
     }
   };
 
@@ -51,7 +63,11 @@ const PdfListComponent = ({ id, file_name, created_at, user_id }: PDFFile) => {
         <CardTitle className="truncate">{file_name}</CardTitle>
         <CardDescription>{formattedDate.toLocaleString()}</CardDescription>
         <CardAction>
-          <Trash className="size-5 hover:text-red-700" onClick={deletePdf} />
+          {isDeleting ? (
+            <LoaderCircle className="size-5 text-red-700 animate-spin" />
+          ) : (
+            <Trash className="size-5 hover:text-red-700" onClick={deletePdf} />
+          )}
         </CardAction>
       </CardHeader>
     </Card>
--- src/components/PdfListView.tsx
+++ src/components/PdfListView.tsx
@@ -12,65 +12,73 @@ const PdfList = ({ id }: PdfListProps) => {
   const [pdfs, setPdfs] = useState<PDFFile[]>([]);
   const [pdfsIsLoading, setPdfsIsLoading] = useState(true);
 
-  useEffect(() => {
+  const fetchPDFs = async () => {
     if (!id) return;
 
-    const fetchPDFs = async () => {
-      try {
-        const res = await fetch("/api/pdf", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ user_id: id }),
-        });
+    try {
+      const res = await fetch("/api/pdf", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user_id: id }),
+      });
 
-        const json = await res.json();
+      const json = await res.json();
 
-        if (!res.ok) {
-          throw new Error(json.error || "Failed to fetch PDFs");
-        }
-
-        setPdfs(json.files);
-      } catch (error) {
-        console.error("Error fetching PDFs:", error);
-      } finally {
-        setPdfsIsLoading(false);
+      if (!res.ok) {
+        throw new Error(json.error || "Failed to fetch PDFs");
       }
-    };
+
+      setPdfs(json.files);
+    } catch (error) {
+      console.error("Error fetching PDFs:", error);
+    } finally {
+      setPdfsIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchPDFs();
   }, [id]);
 
   return (
     <section className="mt-10">
-      <div className="grid grid-cols-4 items-center gap-4">
+      <div className="grid sm:grid-cols-4 grid-cols-2 items-center gap-4">
         {pdfs.map((pdf, index) => (
-            <PdfListComponent key={index} id={pdf.id} file_name={pdf.file_name} created_at={pdf.created_at}user_id={pdf.user_id}/>
+          <PdfListComponent
+            key={index}
+            id={pdf.id}
+            file_name={pdf.file_name}
+            created_at={pdf.created_at}
+            user_id={pdf.user_id}
+            onDelete={fetchPDFs}
+          />
         ))}
       </div>
-        {/* if no pdf list is there */}
-        {pdfs.length === 0 && !pdfsIsLoading && (
-          <div className="text-center mb-8 mt-30">
-            <div className="w-16 h-16 mx-auto mb-4 bg-gray-100 dark:bg-gray-300 rounded-full flex items-center justify-center">
-              <FileText
-                className="text-gray-400 animate-pulse dark:text-gray-700"
-                size={32}
-              />
-            </div>
-            <h2 className="text-xl font-semibold text-gray-900 dark:text-white/80 mb-2">
-              No PDF uploaded
-            </h2>
-            <p className="text-gray-500 dark:text-gray-400">
-              Upload a PDF file to get started
-            </p>
+      {/* if no pdf list is there */}
+      {pdfs.length === 0 && !pdfsIsLoading && (
+        <div className="text-center mb-8 mt-30">
+          <div className="w-16 h-16 mx-auto mb-4 bg-gray-100 dark:bg-gray-300 rounded-full flex items-center justify-center">
+            <FileText
+              className="text-gray-400 animate-pulse dark:text-gray-700"
+              size={32}
+            />
           </div>
-        )}
-        {/* loading skeleton */}
-        {pdfsIsLoading && (
-          <Skeleton
-            height={100}
-            count={3}
-            className="dark:opacity-5 animate-pulse"
-          />
-        )}
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white/80 mb-2">
+            No PDF uploaded
+          </h2>
+          <p className="text-gray-500 dark:text-gray-400">
+            Upload a PDF file to get started
+          </p>
+        </div>
+      )}
+      {/* loading skeleton */}
+      {pdfsIsLoading && (
+        <Skeleton
+          height={100}
+          count={3}
+          className="dark:opacity-5 animate-pulse"
+        />
+      )}
     </section>
   );
 };
